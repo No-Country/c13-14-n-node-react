4bb3bc1181aa1b6512820036d0a0f711721584a2
Jackson avila paez <arenca44@gmail.com>
2023-08-31 16:26:58 -0500

    validacion ok user

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -34,43 +34,26 @@ const getAllUsers = catchAsync(async (req, res, next) => {
 });
 
 const createUser = catchAsync(async (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
-  const validate = await User.findOne({ email, name }) || null;
+  const validate = await User.findOne({ email }) || null;
 
-  const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{3,254})+$/;
 
-  const validPassword = /^(?=.*[a-zA-Z])(?=.*\d).{5,10}$/;
-
-  const validName = /^[a-zA-Z\s]{5,}$/;
-
-  if(validate!==null){
+  if (validate !== null) {
     return res.status(409).json({ message: "User exist" });
   }
 
-  if(!(validEmail.test(email))){
-    return res.status(409).json({ message: "Email not Valid" });
-  }
-
-  if(!(validPassword.test(password))){
-    return res.status(409).json({ message: "Password not Valid" });
-  }
-
-  if(!(validName.test(name))){
-    return res.status(409).json({ message: "Name not Valid" });
-  }
 
   const salt = await bcrypt.genSalt(12);
   const hashPassword = await bcrypt.hash(password, salt);
 
   const newUser = await User.create({
-    name,
     email,
     password: hashPassword,
     status: false
   });
 
-  const token = jwt.sign({id: newUser.name, email: newUser.email}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
+  const token = jwt.sign({ id: newUser.name, email: newUser.email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 
   // Remove password from response
   newUser.password = undefined;
@@ -131,9 +114,9 @@ const login = catchAsync(async (req, res, next) => {
 
   // Compare password with db
 
-  if(!user){
+  if (!user) {
     return res.status(401).json({ message: "Invalid email" });
-  }else if(!(await bcrypt.compare(password, user.password))){
+  } else if (!(await bcrypt.compare(password, user.password))) {
     return res.status(401).json({ message: "Invalid password" });
   }
 
@@ -151,47 +134,47 @@ const checkToken = catchAsync(async (req, res, next) => {
   res.status(200).json({ user: req.sessionUser });
 });
 
-const validateTokenSession = catchAsync(async (req, res, next) =>{
-  
+const validateTokenSession = catchAsync(async (req, res, next) => {
+
   const token = req.params.token;
-  
-  if(!token){
+
+  if (!token) {
     return res.status(401).json({ message: 'Token missing' }); // res.redirect(url);
   }
 
-  try{
+  try {
 
     const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
 
     console.log(decodeToken);
 
-    const validateUser = User.updateOne({ email: decodeToken.email }, {status: true})
-    .then(()=>{
+    const validateUser = User.updateOne({ email: decodeToken.email }, { status: true })
+      .then(() => {
 
-      const mailOptions = {
-        from: process.env.MAIL_NODEMAILER,
-        to: decodeToken.email,
-        subject: `¡Bienvenido ${decodeToken.id}!`,
-        text: `Su Cuenta fue activada con exito!`
-      };
+        const mailOptions = {
+          from: process.env.MAIL_NODEMAILER,
+          to: decodeToken.email,
+          subject: `¡Bienvenido ${decodeToken.id}!`,
+          text: `Su Cuenta fue activada con exito!`
+        };
 
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error('Error al enviar el correo:', error);
-        } else {
-          console.log('Correo enviado:', info.response);
-        }
-      });
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error('Error al enviar el correo:', error);
+          } else {
+            console.log('Correo enviado:', info.response);
+          }
+        });
 
-      res.status(200).json({message: 'Account activated'})
-    }) // res.redirect(url);
-    .catch(error=>{
-      console.log(error)
-      res.status(404).json({message: 'User not found'})
-    }) // res.redirect(url);
-    
+        res.status(200).json({ message: 'Account activated' })
+      }) // res.redirect(url);
+      .catch(error => {
+        console.log(error)
+        res.status(404).json({ message: 'User not found' })
+      }) // res.redirect(url);
+
   } catch (error) {
-    return res.status(401).json({message: 'Invalid token'}); // res.redirect(url);
+    return res.status(401).json({ message: 'Invalid token' }); // res.redirect(url);
   }
 
 });
