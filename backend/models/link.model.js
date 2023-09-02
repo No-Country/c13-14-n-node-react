92b6031d7e0831783901bbb88ccdfb9e1629061f
Jackson avila paez <arenca44@gmail.com>
2023-09-02 12:07:13 -0500

    se agrega backend

---
+++ backend/models/link.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const linkSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profile: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Profile",
+    required: true,
+  },
+  urlEnlace: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  icon: {
+    type: String, //Por definir
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Link = mongoose.model('Link', linkSchema);
+
+module.exports = { Link };
